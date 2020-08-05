f972fc8ce4af3caaff78a4ea1d8b222da3ae6de2
YiYing He <yiying@secondstate.io>
2020-08-05 20:38:34 +0800

    [AST] Add new bulk-memory and reference-types instructions. 1. Reference instructions:  * ref.null  * ref.is_null  * ref.func 2. Table instructions:  * table.get  * table.set  * table.init  * elem.drop  * table.copy  * table.grow  * table.size  * table.fill 3. Memory instructions:  * memory.init  * data.drop  * memory.copy  * memory.fill 4. Parametric instructions:  * select t

--- lib/aot/compiler.cpp
+++ lib/aot/compiler.cpp
@@ -522,12 +522,25 @@ public:
       __builtin_unreachable();
     }
   }
+  Expect<void> compile(const AST::ReferenceInstruction &Instr) {
+    switch (Instr.getOpCode()) {
+    case OpCode::Ref__null:
+    case OpCode::Ref__is_null:
+    case OpCode::Ref__func:
+      /// TODO: Implement this.
+      return Unexpect(ErrCode::InvalidGrammar);
+    default:
+      __builtin_unreachable();
+    }
+    return {};
+  }
   Expect<void> compile(const AST::ParametricInstruction &Instr) {
     switch (Instr.getOpCode()) {
     case OpCode::Drop:
       stackPop();
       break;
-    case OpCode::Select: {
+    case OpCode::Select:
+    case OpCode::Select_t: {
       llvm::Value *Cond = Builder.CreateICmpNE(stackPop(), Builder.getInt32(0));
       llvm::Value *False = stackPop();
       llvm::Value *True = stackPop();
@@ -571,6 +584,23 @@ public:
       __builtin_unreachable();
     }
 
+    return {};
+  }
+  Expect<void> compile(const AST::TableInstruction &Instr) {
+    switch (Instr.getOpCode()) {
+    case OpCode::Table__get:
+    case OpCode::Table__set:
+    case OpCode::Table__init:
+    case OpCode::Elem__drop:
+    case OpCode::Table__copy:
+    case OpCode::Table__grow:
+    case OpCode::Table__size:
+    case OpCode::Table__fill:
+      /// TODO: Implement this.
+      return Unexpect(ErrCode::InvalidGrammar);
+    default:
+      __builtin_unreachable();
+    }
     return {};
   }
   Expect<void> compile(const AST::MemoryInstruction &Instr) {
